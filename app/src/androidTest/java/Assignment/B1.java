4cf9854506ca9bffc03920aa8ff7aeda1c1fb5f7
Saugata Roy Arghya <saugataroyarghya@gmail.com>
2023-03-11 16:23:53 +0600

    Hello there

--- app/src/androidTest/java/Assignment/B1.java
+++ app/src/androidTest/java/Assignment/B1.java
@@ -2,16 +2,14 @@ package Assignment;
 
 public class B1 {
     String a, b;
-    float number;
-
+    double conflicting_number;
     public B1(String a, String b, float number, double conflicting_number) {
         this.a = a;
         this.b = b;
-        this.number = number;
         this.conflicting_number = conflicting_number;
     }
 
-    double conflicting_number;
+
 
     public double getConflicting_number() {
         return conflicting_number;
@@ -20,7 +18,7 @@ public class B1 {
     public void setConflicting_number(double conflicting_number) {
         this.conflicting_number = conflicting_number;
     }
-    
+
 
     public String getA() {
         return a;
