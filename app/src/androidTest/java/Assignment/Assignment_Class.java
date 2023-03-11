22f0681fec0cf439580567eef220048c5b5aa90d
Saugata Roy Arghya <saugataroyarghya@gmail.com>
2023-03-11 16:04:12 +0600

    Java Class added. Constructor, Getter and Setter created.

---
+++ app/src/androidTest/java/Assignment/Assignment_Class.java
@@ -0,0 +1,27 @@
+package Assignment;
+
+public class Assignment_Class {
+    int a;
+    String b;
+
+    public Assignment_Class(int a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+}
