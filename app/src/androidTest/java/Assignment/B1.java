4f75033da6df9e723218e87bfea642f0446f37bd
Saugata Roy Arghya <saugataroyarghya@gmail.com>
2023-03-11 16:17:50 +0600

    Changed the B1.java file.

--- app/src/androidTest/java/Assignment/B1.java
+++ app/src/androidTest/java/Assignment/B1.java
@@ -3,6 +3,15 @@ package Assignment;
 public class B1 {
     String a, b;
     float number;
+    double another_number;
+
+    public double getAnother_number() {
+        return another_number;
+    }
+
+    public void setAnother_number(double another_number) {
+        this.another_number = another_number;
+    }
 
     public B1(String a, String b, float number) {
         this.a = a;
